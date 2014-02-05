2430f7f479e08cefaf1bb16c2e6e8593626b151e
John Cinnamond <jc@panagile.com>
2014-02-05 15:06:13 +0000

    Allow contract_double to take return values for stubbed methods.

--- lib/lawyer/rspec/contract_double.rb
+++ lib/lawyer/rspec/contract_double.rb
@@ -1,10 +1,15 @@
 module Lawyer
   module RSpec
     module ContractDouble
-      def contract_double(contract)
+      def contract_double(contract, return_values = {})
         methods = {}
         contract.clauses.each do |clause|
-          methods[clause.name] = true
+          return_value = if return_values.has_key?(clause.name.to_sym)
+                           return_values[clause.name.to_sym]
+                         else
+                           true
+                         end
+          methods[clause.name] = return_value
         end
         double(contract.name, methods)
       end
